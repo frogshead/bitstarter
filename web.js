e105dea1c728251cad130938853d7595919d423a
Mikko Viitamaki <mikko.viitamaki@gmail.com>
2013-07-14 17:59:17 +0000

    once again

--- web.js
+++ web.js
@@ -1,12 +1,12 @@
 
 var express = require('express');
 var fs = require('fs');
-var buffer = require('buffer');
+var buffer = new Buffer(256);
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   /*response.send('Hello World 2!');*/
-	response.send(buffer.toString(fs.readFileSync('./index.html')));
+	response.send(fs.readFileSync('./index.html').toString());
 });
 
 var port = process.env.PORT || 5000;
